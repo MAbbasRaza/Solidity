a02f48ae4b10129cd42a7aa636414329e570ee03
MAbbasRaza <96629590+MAbbasRaza@users.noreply.github.com>
2022-01-21 02:49:16 +0500

    Inbox Contract

---
+++ inbox/compile.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+//Getting Contract Path And Source For Compiling
+
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
+
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
